fc65c05a50444db53ed7701372a4c64925b8021a
LeaBroudo <lrb2167@columbia.edu>
2020-12-20 00:36:04 -0500

    start2

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ window.onload = function()
     {
         render()
         {
-            //return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
-            return (<AwesomeButton type="primary">Primary</AwesomeButton>);
+            return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
+            //return (<h1>Hello</h1><AwesomeButton type="primary">Primary</AwesomeButton>);
         }
     }
 
